d57c3e43d362092331ced8c967bd960271535f5e
wesbragagt <wesbragagt@gmail.com>
2023-12-15 11:29:17 -0600

    chore: build package setup

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,14 @@
+#! /usr/bin/env node
+
 import fs from 'fs';
+import { TableContent } from './table-content';
+import path from 'path';
 
 function main(){
-
+  const filetarget = process.argv[2] || 'README.md';
+  const contentRead = fs.readFileSync(path.join(process.cwd(), filetarget), 'utf8');
+  const tableContent = new TableContent(contentRead).make();
+  process.stdout.write(tableContent.join('\n'));
 }
 
 main();
-
-function parseReadFileContent(content: string){
-  const matches = {
-    h1(line: string){
-      return line.match(/^(#)(w|d)/);
-    } ,
-    h2(line: string){
-      return line.match(/^(##)(w|d)/);
-    },
-  }
-  const lines = content.split('\n');
-}
